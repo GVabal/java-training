0bc2e3ef3259ac22e857179ccd97d203b9e3099d
Gabrielius Vabalas <gvabal@gmail.com>
2022-01-31 23:31:06 +0200

    Data types

--- src/main/java/dev/vabalas/DataTypes.java
+++ src/main/java/dev/vabalas/DataTypes.java
@@ -3,15 +3,31 @@ package dev.vabalas;
 public class DataTypes {
 
     public void primitives() {
-
+        char pChar = Character.MAX_VALUE;
+        byte pByte = Byte.MAX_VALUE;
+        short pShort = Short.MAX_VALUE;
+        int pInt = Integer.MAX_VALUE;
+        long pLong = Long.MAX_VALUE;
+        float pFloat = Float.MAX_VALUE;
+        double pDouble = Double.MAX_VALUE;
     }
 
     public void wrapperClasses() {
-
+        Character wChar = Character.MIN_VALUE;
+        Byte wByte = Byte.MIN_VALUE;
+        Short wShort = Short.MIN_VALUE;
+        Integer wInt = Integer.MIN_VALUE;
+        Long wLong = Long.MIN_VALUE;
+        Float wFloat = Float.MIN_VALUE;
+        Double wDouble = Double.MIN_VALUE;
     }
 
     public void basicOperations() {
-
+        System.out.println(10 + 2);
+        System.out.println(10 - 2);
+        System.out.println(10 / 2);
+        System.out.println(10 % 2);
+        System.out.println(10 * 2);
     }
 
     /**
@@ -19,19 +35,17 @@ public class DataTypes {
      *        char->int
      * byte->short->int->long->float->double
      */
-    public void typePromotions() {
-
-    }
+    public void typePromotions() {}
 
     public void stringOperations() {
-
+        System.out.println("repeat ".repeat(10));
+        System.out.println("".isBlank());
+        System.out.println("hello\nworld".lines());
+        System.out.println("  strip   ".strip());
     }
 
     public void stringBuilderOperations() {
-
-    }
-
-    public void var() {
-
+        var stringBuilder = new StringBuilder();
+        System.out.println(stringBuilder.append("yay").toString());
     }
 }
